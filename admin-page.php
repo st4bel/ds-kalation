a461f63d1288470014000fff453e06ef444a2493
Max Stapelfeld <nasak.o@hotmail.de>
2016-09-06 22:02:53 +0200

    first join sql request test

--- admin-page.php
+++ admin-page.php
@@ -23,11 +23,11 @@
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
-        echo "Connected successfully <br>";
+        echo "Connected successfully"."<br>";
 
-        $sql = "SELECT * FROM `groups` WHERE `id` =1";
+        $sql = "SELECT * FROM groups,members WHERE groups.id = members.group_id and members.user_id = ".get_current_user_id();
         $result = $conn->query($sql);
-
+        echo "Die haben Zugang zu den Gruppen:<br>";
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
@@ -37,6 +37,18 @@
             echo "0 results";
         }
 
+        /*echo "<br><br>";
+        $sql = "SELECT * FROM `members` WHERE `user_id` = ".get_current_user_id();
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo "user_id: " . $row["user_id"]. " - group_id: " . $row["group_id"]. "<br>";
+            }
+        } else {
+            echo "0 results";
+        }*/
 
         $conn->close();
     ?>
